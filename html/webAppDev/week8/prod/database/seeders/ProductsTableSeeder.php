4ad288b7313160b0ce331ce11c3ae7f15a33af22
Adiaworks <lyuwen0121@gmail.com>
2021-09-19 22:12:40 +1000

    week8 task2

---
+++ html/webAppDev/week8/prod/database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+/* this line allows us to access to DB class*/
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('products')->insert([
+            'name'=> 'iPhone 20',
+            'price'=> 6009,
+            'manufacturer_id'=> 1,
+            'updated_at'=> DB::raw('CURRENT_TIMESTAMP'),
+        ]);
+        DB::table('products')->insert([
+            'name'=> 'Note 21',
+            'price'=> 5674,
+            'manufacturer_id'=> 1,
+            'updated_at'=> DB::raw('CURRENT_TIMESTAMP'),
+        ]);
+        DB::table('products')->insert([
+            'name'=> 'Surface Pro',
+            'price'=> 1234,
+            'manufacturer_id'=> 1,
+            'updated_at'=> DB::raw('CURRENT_TIMESTAMP'),
+        ]);
+
+    }
+}
