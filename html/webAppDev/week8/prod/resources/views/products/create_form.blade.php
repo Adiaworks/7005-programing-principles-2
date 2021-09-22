4aa42a025288509ba5da5bf587ff3a29d7baa08a
Adiaworks <lyuwen0121@gmail.com>
2021-09-22 10:09:48 +1000

    week8 done

---
+++ html/webAppDev/week8/prod/resources/views/products/create_form.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title')
+    Product Create
+@endsection
+
+@section('content')
+    <form method="POST" action='{{url("product")}}'> <!--goes to the store function in the controller -->
+        {{csrf_field()}}
+        <p><label>Name: </label><input type="text" name="name"></p>
+        <p><label>Price: </label><input type="text" name="price"></p>
+        <p><select name="manufacturer">
+        @foreach ($manufacturers as $manufacturer)
+            <option value="{{$manufacturer->id}}">{{$manufacturer->name}}</option>
+        @endforeach
+        </select></p>
+        <input type="submit" value="Create"> 
+    </form>
+@endsection
