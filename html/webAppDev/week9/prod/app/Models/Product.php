a150718454375a3c8a3ea88abb48f0c6d691c2c9
Adiaworks <lyuwen0121@gmail.com>
2021-09-24 00:41:03 +1000

    week 9 task1 done

---
+++ html/webAppDev/week9/prod/app/Models/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;//this line allows us to inherit Model class to our class Product
+
+class Product extends Model //our Product class equals to a child class of Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'price'];
+    function manufacturer() {
+        return $this->belongsTo('App\Models\Manufacturer');
+        }
+}
+//to call the class Product, we need to use the full path like App\Medels\Product, or type "use App\Medels\Product;"at the begining of a file
