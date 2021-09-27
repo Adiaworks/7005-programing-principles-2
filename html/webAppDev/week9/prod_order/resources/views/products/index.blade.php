7cb5bedd342392b039337384ecf2ec67592f6e93
Adiaworks <lyuwen0121@gmail.com>
2021-09-27 20:44:01 +1000

    week9 task4 done

--- html/webAppDev/week9/prod_order/resources/views/products/index.blade.php
+++ html/webAppDev/week9/prod_order/resources/views/products/index.blade.php
@@ -11,8 +11,6 @@
         <a href="product/{{$product->id}}"><li>{{$product->name}}</li></a>
     @endforeach
 </ul>
-<form method="GET" action= '{{url("product/create")}}'>
-        {{csrf_field()}} 
-        <input type="submit" value="Create">
-</form>
+
 @endsection
+
--- html/webAppDev/week9/prod_order/resources/views/products/show.blade.php
+++ html/webAppDev/week9/prod_order/resources/views/products/show.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.master')
 @section('content')
-<h1>{{$product->name}}</h1>
-<p>{{$product->price}}</p>
-<p>{{$product->manufacturer->name}}</p>
-<p><a href=' {{url("product/$product->id/edit")}}'>Edit</a></p>
-<p>
-    <form method="POST" action= '{{url("product/$product->id")}}'>
-        {{csrf_field()}}
-        {{ method_field('DELETE') }} <!--html does not support DELETE method, so this line can use hidden html to conduct the delete method -->
-        <input type="submit" value="Delete">
-    </form>
-</p>
+    <h1>{{$product->name}}</h1>
+    <p>{{$product->price}}</p>
+    <p>{{$product->manufacturer->name}}</p>
+    @auth <!--- user is logged in --->
+        <p><a href=' {{url("product/$product->id/edit")}}'>Edit</a></p>
+        <p>
+            <form method="GET" action= '{{url("product/create")}}'>
+                {{csrf_field()}} 
+                <input type="submit" value="Create">
+            </form>
+            <form method="POST" action= '{{url("product/$product->id")}}'>
+                {{csrf_field()}}
+                {{ method_field('DELETE') }} <!--html does not support DELETE method, so this line can use hidden html to conduct the delete method -->
+                <input type="submit" value="Delete">
+            </form>
+        </p>
+    @endauth
 @endsection
